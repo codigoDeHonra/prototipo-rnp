2ca1aaec4d05f2ca876cdf6b1faff9caf5060a61
Luiz F. Pinheiro <luizfelipenet@hotmail.com>
2020-02-15 12:57:21 -0300

    [UPDATE] Melhorias na tela de Carrinho

--- src/components/ItemCarrinhoComponent.vue
+++ src/components/ItemCarrinhoComponent.vue
@@ -2,7 +2,7 @@
   <div class="grey lighten-4 pa-3">
     <v-card class="d-inline-block mx-auto" width="100%">
       <v-container>
-        <v-row justify="space-between text-left pl-0">
+        <v-row justify="space-between" class="ext-left pl-0">
           <v-col cols="1">
             <v-img
               src="https://www.microtelit.com.br/wp-content/uploads/2019/10/Logo-Office-365.jpeg"
@@ -15,9 +15,9 @@
                 <div
                   class="subtitle-1 font-weight-bold"
                 >99 Contas Office 365 for Education para usuários finais</div>
-                <div class="overline ma-0">
-                  <v-btn text small class="blue--text">Alterar</v-btn>
-                  <v-btn text small class="blue--text">Remover</v-btn>
+                <div class="overline ma-0 pa-0">
+                  <v-btn text small class="blue--text pl-0">Alterar</v-btn>
+                  <v-btn text small class="blue--text pl-0">Remover</v-btn>
                 </div>
               </v-list-item-content>
             </v-list-item>
--- src/components/catalogo/Carrinho.vue
+++ src/components/catalogo/Carrinho.vue
@@ -1,9 +1,49 @@
 <template>
   <div class="grey lighten-4 grey--text pa-4">
-    <div class="display-1 font-weight-thin">Carrinho de Compras</div>
-    <div class="title font-weight-regular pa-2">Serviços SaaS</div>
-    <v-divider class="pb-3"></v-divider>
-      <ItemCarrinhoComponent v-for="item in 4" :key="item"></ItemCarrinhoComponent>
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="11">
+          <div class="display-1 font-weight-thin pa-2">Carrinho de Compras</div>
+          <div class="title font-weight-regular pa-2">Serviços SaaS</div>
+          <v-divider class="pb-3"></v-divider>
+          <ItemCarrinhoComponent v-for="item in 2" :key="item"></ItemCarrinhoComponent>
+
+          <div class="title font-weight-regular pa-2">Serviços IaaS</div>
+          <v-divider class="pb-3"></v-divider>
+          <ItemCarrinhoComponent></ItemCarrinhoComponent>
+          <v-row justify="end">
+            <v-col cols="12">
+              <v-btn text small color="blue lighten-1">Adicionar mais itens</v-btn>
+            </v-col>
+            <v-col cols="2">
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="subheading">Total de impostos</v-list-item-title>
+                  <v-list-item-title class="subheading font-weight-bold">R$ 0,00</v-list-item-title>
+                  <v-list-item-title class="caption font-weight-thin"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+            <v-col cols="2">
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <v-list-item-title class="subheading">Valor Total</v-list-item-title>
+                  <v-list-item-title class="subheading font-weight-bold">R$ 1.782,00</v-list-item-title>
+                  <v-list-item-title class="caption font-weight-thin">Cobrança Mensal</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+            <v-col cols="4" offset="6">
+              <v-list-item>
+                <v-list-item-title>
+                  <v-btn block color="blue lighten-1" class="white--text">Prosseguir para pagamento</v-btn>
+                </v-list-item-title>
+              </v-list-item>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
@@ -15,7 +55,6 @@ export default {
   components: {
     ItemCarrinhoComponent
   },
-  data: () => ({
-  })
+  data: () => ({})
 };
 </script>
